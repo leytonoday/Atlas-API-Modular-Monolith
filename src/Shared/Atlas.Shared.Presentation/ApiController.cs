b9f196f55fe9cfa4ff217fa68fe30ccac20aaa55
Leyton O'Day <leytonodayabc123@gmail.com>
2024-03-23 22:54:26 +0000

    Added shared presentation classes

---
+++ src/Shared/Atlas.Shared.Presentation/ApiController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Atlas.Shared.Presentation;
+
+/// <summary>
+/// Base class for API controllers providing common functionality and error handling.
+/// </summary>
+[ApiController]
+public abstract class ApiController : ControllerBase
+{
+    private ISender _sender = null!;
+
+    /// <summary>
+    /// Gets the sender.
+    /// </summary>
+    protected ISender Sender => _sender ??= HttpContext.RequestServices.GetService<ISender>()!;
+}
+
