050678106749430182e18be5721c99d129fa1014
Leyton O'Day <leytonodayabc123@gmail.com>
2024-04-03 00:33:13 +0100

    Cleaned up HandleStripeWebhookCommandHandler

--- src/Modules/Plans/Atlas.Plans.Application/CQRS/Webhooks/Commands/HandleStripeWebhook/HandleStripeWebhookCommandHandler.cs
+++ src/Modules/Plans/Atlas.Plans.Application/CQRS/Webhooks/Commands/HandleStripeWebhook/HandleStripeWebhookCommandHandler.cs
@@ -70,7 +70,8 @@ internal sealed class HandleStripeWebhookCommandHandler(
         User user = await userRepository.GetByIdAsync(stripeCustomer.UserId, true, cancellationToken)
           ?? throw new ErrorException(UsersDomainErrors.User.UserNotFound);
 
-        // Set the user's planId to the planId that was specified in the subscription's metadata
+        // Set the user's planId to the planId that was specified in the subscription's metadata.
+        // The result of this is that this user should have access to all of this Plan's features
         User.SetPlanId(user, new Guid(planId));
 
         await AddCardFingerprintIfNewAsync(invoice, subscription, cancellationToken);
@@ -106,7 +107,7 @@ internal sealed class HandleStripeWebhookCommandHandler(
         }
 
         // The approach above means that a user can't use the same card for a trial, even if that card has been used to make regular non-trial payments.
-        // For example, if another user has used that card at all, for any Clipboard TTS related payment, it is now ineligable for the trial.
+        // For example, if another user has used that card at all, for any Atlas related payment, it is now ineligable for the trial.
 
         StripeCardFingerprint? existingCardFingerprint = await stripeCardFingerprintRepository.GetByFingerprintAsync(cardFingerprint, false, cancellationToken);
 
@@ -143,7 +144,7 @@ internal sealed class HandleStripeWebhookCommandHandler(
 
         // TODO - Send email to user, AND specify the reason for the failure.
 
-        // Clear the user's planId
+        // Clear the user's planId. The result of clearing this planId means the user cannot access any of that Plan's features anymore
         User.SetPlanId(user, null);
 
         await userRepository.UpdateAsync(user, cancellationToken);
@@ -176,7 +177,7 @@ internal sealed class HandleStripeWebhookCommandHandler(
         await stripeService.VoidAllOpenInvoicesAsync(stripeCustomer.StripeCustomerId, cancellationToken);
     }
 
-        /// <summary>
+    /// <summary>
     /// Handles the event when a <see cref="Stripe.Refund"/> fails. We must communicate this to both the support email, and the customer themselves.
     /// </summary>
     /// <param name="webhookEvent">The event.</param>
@@ -187,16 +188,16 @@ internal sealed class HandleStripeWebhookCommandHandler(
         if (webhookEvent.Data.Object is not Refund refund)
             return;
 
+        if (refund.Status == "succeeded" || refund.Status == "pending" || refund.Status == "canceled")
+            return;
+
         // Using the attached Charge, get the customer Id, and then using that, get the User
         Charge charge = await stripeService.ChargeService.GetAsync(refund.ChargeId, null, null, cancellationToken);
 
         StripeCustomer? stripeCustomer = await stripeCustomerRepository.GetByStripeCustomerId(charge.CustomerId, false, cancellationToken)
             ?? throw new ErrorException(PlansDomainErrors.StripeCustomer.StripeCustomerNotFound);
 
-        if (refund.Status == "succeeded" || refund.Status == "pending" || refund.Status == "canceled")
-            return;
-
-        //// Alert the user, and the support email of the refund failure
+        // Alert the user, and the support email of the refund failure
         await supportNotifierService.AttemptNotifyAsync(@$"Refund for {refund.Amount / 100.0} {refund.Currency.ToUpper()} failed for user with Id {stripeCustomer.UserId}. Reason: '{refund.FailureReason}'.", cancellationToken);
 
         // Before a refund is processed, we directly remove the credit from their account that downgrading would've given them. 
